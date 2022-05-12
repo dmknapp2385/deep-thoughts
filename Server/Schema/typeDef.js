63c3c8f372254a0f2f099301556673d243b4a979
dmknapp2385 <dmknapp2385@gmail.com>
2022-05-12 10:23:00 -0700

    thought, users, user typedefs and resolvers created

--- Server/Schema/typeDef.js
+++ Server/Schema/typeDef.js
@@ -3,16 +3,35 @@ const { gql } = require('apollo-server-express');
 
 // create typeDefs
 const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    friendCount: Int
+    thoughts: [Thought]
+    friends: [User]
+  }
+  
   type Thought {
     _id: ID
     thoughtText: String
     createdAt: String
     username: String
     reactionCount: Int
+    reactions: [Reaction]
+  }
+
+  type Reaction {
+    _id: ID
+    reactionBody: String
+    createdAt: String
+    username: String
   }
 
   type Query {
-    thoughts: [Thought]
+    users: [User]
+    user(username: String!): User
+    thought(_id: ID!): Thought
     thoughts(username: String): [Thought]
   }
 `;
