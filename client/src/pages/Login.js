6f5b1fb98763f2be2edce35ccd449efe83272c60
dmknapp2385 <dmknapp2385@gmail.com>
2022-06-11 14:14:17 -0700

    auth added to all api requests, login and signup mutations

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,6 +1,12 @@
 import React, { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../utils/mutations';
+import Auth from '../utils/auth';
 
 const Login = (props) => {
+
+  const [login, {error}] = useMutation(LOGIN_USER);
+
   const [formState, setFormState] = useState({ email: '', password: '' });
 
   // update state based on form input changes
@@ -16,6 +22,15 @@ const Login = (props) => {
   // submit form
   const handleFormSubmit = async (event) => {
     event.preventDefault();
+
+    try{
+      const { data } = await login({
+        variables: {...formState}
+      });
+      Auth.login(data.login.token)
+    } catch(e) {
+      console.error(e)
+    }
 
     // clear form values
     setFormState({
@@ -52,6 +67,7 @@ const Login = (props) => {
               <button className='btn d-block w-100' type='submit'>
                 Submit
               </button>
+              {error && <div>Login Failed</div>}
             </form>
           </div>
         </div>
