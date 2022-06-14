4eabc48baa5ab2d27cfc1f38535f65825bc5a256
dmknapp2385 <dmknapp2385@gmail.com>
2022-06-14 10:00:01 -0700

    add thought mutation added

--- client/src/components/ThoughtForm/index.js
+++ client/src/components/ThoughtForm/index.js
@@ -1,19 +1,71 @@
 import React, { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import { ADD_THOUGHT } from '../../utils/mutations';
+import { QUERY_THOUGHTS, QUERY_ME } from '../../utils/queries';
 
 const ThoughtForm = () => {
-    const [thoughtText, setText] = useState('');
-    const [characterCount, setCharacterCount] = useState(0);
+  const [thoughtText, setText] = useState('');
+  const [characterCount, setCharacterCount] = useState(0);
 
-    const handleChange = event => {
-        if(event.target.value.length <= 280) {
-            setText(event.target.value);
-            setCharacterCount(event.target.value.length)
-        }
+  const [addThought, { error }] = useMutation(ADD_THOUGHT, {
+    update(cache, { data: { addThought } }) {
+      // could potentially not exist yet, so wrap in a try/catch
+      try {
+        // update me array's cache
+        const { me } = cache.readQuery({ query: QUERY_ME });
+        cache.writeQuery({
+          query: QUERY_ME,
+          data: { me: { ...me, thoughts: [...me.thoughts, addThought] } }
+        });
+      } catch (e) {
+        console.warn('First thought insertion by user!');
+      }
+      // read what's currently in the cache
+      const { thoughts } = cache.readQuery({ query: QUERY_THOUGHTS });
+
+      // prepend the newest thought to the front of the array
+      cache.writeQuery({
+        query: QUERY_THOUGHTS,
+        data: { thoughts: [addThought, ...thoughts] }
+      });
     }
+  });
+
+  const handleChange = (event) => {
+    if (event.target.value.length <= 280) {
+      setText(event.target.value);
+      setCharacterCount(event.target.value.length);
+    }
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      await addThought({
+        variables: { thoughtText }
+      });
+
+      //clear form value
+      setText('');
+      setCharacterCount(0);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
   return (
     <div>
-      <p className={`m-0 ${characterCount === 280? 'text-error':''}`}>Character Count: {characterCount}/280</p>
-      <form className="flex-row justify-center justify-space-between-md align-stretch">
+      <p
+        className={`m-0 ${characterCount === 280 || error ? 'text-error' : ''}`}
+      >
+        Character Count: {characterCount}/280{' '}
+        {error && <span className="ml-2">Something went wrong...</span>}
+      </p>
+      <form
+        className="flex-row justify-center justify-space-between-md align-stretch"
+        onSubmit={handleSubmit}
+      >
         <textarea
           placeholder="Here's a new thought..."
           value={thoughtText}
@@ -27,6 +79,5 @@ const ThoughtForm = () => {
     </div>
   );
 };
-
 
 export default ThoughtForm;
